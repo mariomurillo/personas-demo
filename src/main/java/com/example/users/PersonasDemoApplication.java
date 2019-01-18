c7dcef8dd6837ccd24c494889bee8dd6e81b3f0a
Mario.Murillo <mario.murillo@MacBook-Pro-de-Admin-4.local>
2019-01-18 15:15:04 -0500

    start project

---
+++ src/main/java/com/example/users/PersonasDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasDemoApplication.class, args);
+	}
+
+}
+
